2e6a59ff62f2ffee4b8bb73d03e9fe247829d114
Joni Oliveira <joni.oliveira89@gmail.com>
2021-10-30 15:37:19 +0100

    feat: add translate command (#5)

--- internal/cmd/translate.go
+++ internal/cmd/translate.go
@@ -1,1 +1,32 @@
 package cmd
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/jonioliveira/tlt/internal/credentials"
+	"github.com/jonioliveira/tlt/internal/translate"
+	"github.com/spf13/cobra"
+)
+
+func GetTranslateCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "translate",
+		Short: "translate sentence",
+		Long:  "translate sentence",
+		Run: func(cmd *cobra.Command, args []string) {
+			context := context.Background()
+			credentials, err := credentials.GetEnvCreds("TRANSLATION_API_CREDS")
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+			}
+			translated, err := translate.Do(context, credentials, strings.Join(args, " "), "PT", "EN")
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+			}
+
+			fmt.Printf("%s", translated)
+		},
+	}
+}
